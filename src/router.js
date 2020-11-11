12a4c22ac281c854531a2a62d8a613e0aef3a165
KalebPuniwan <Kalebpuniwan19@gmail.com>
2020-11-11 12:51:57 +0700

    UGD VueCLI-webpack_9667

--- src/router.js
+++ src/router.js
@@ -29,6 +29,13 @@ const router = new VueRouter({
                    name: "Guided",
                    component: importComponent('TodoList/List'),
                },
+              
+               //UGD
+               {
+                path: "/ugd",
+                name: "UGD",
+                component: importComponent('TodoList/ListUGD'),
+               },
            ]
        },
    ]
